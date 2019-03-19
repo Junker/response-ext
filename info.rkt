e69895a576ed7ddf209b984fb8730ed038951995
Junker <dk-junker@ya.ru>
2019-03-19 12:59:54 +0700

    deps fix

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
 
 (define collection "response-ext")
-(define version "0.0.2")
+(define version "0.0.3")
 (define deps '("base" "web-server-lib" "rackunit-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "web-server-doc"))
 (define scribblings '(("scribblings/response-ext.scrbl" ())))
 (define pkg-authors '(junker))
 (define pkg-desc "Extended HTTP response functions for Racket Web Server")
